693e78adff9f444653f173791d88bbba0d85f4d9
Anton Nakonechny <anakonechny@alfabank.ru>
2025-01-15 14:19:56 +0100

    feat(*): deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
+import { ghPages } from "vite-plugin-gh-pages";
 import path from "path";
 
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    base: "test-tree",
+    plugins: [react(), ghPages()],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
